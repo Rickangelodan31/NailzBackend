aad5400beb24cb812a82706b4929b0f8e7961814
Ricardo Watson <Rickangelodan31@gmail.com>
2024-05-08 14:56:09 +0200

    made adjustments in user and designer

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const User = require("../models/User.model");
 const uploader = require("../middlewares/cloudinary.config");
 const { isAuthenticated } = require("../middlewares/route-gaurd.middleware");
+const Designer = require("../models/Designer.model");
 
 // All routes start with /api/users
 
@@ -25,9 +26,11 @@ router.post(
   }
 );
 
-router.get("/", async (req, res) => {
+router.get("/", isAuthenticated, async (req, res) => {
   try {
-    const users = await User.find().populate({
+    const users = await Designer.findOne({
+      vendor: req.tokenPayload.userId,
+    }).populate({
       path: "vendor",
       select: "-hashedPassword",
     }); // Make sure to strip away the password
@@ -37,32 +40,38 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/:id", async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id);
-    if (user) {
-      res.json(user);
-    } else {
-      res.status(404).json({ message: "user not found" });
-    }
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+// router.get("/:id", async (req, res) => {
+//   try {
+//     const user = await User.findById(req.params.id);
+//     if (user) {
+//       res.json(user);
+//     } else {
+//       res.status(404).json({ message: "user not found" });
+//     }
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// });
 
-router.put("/:id", async (req, res) => {
+router.put("/", isAuthenticated, async (req, res) => {
+  console.log(req.body)
   try {
-    const { id } = req.params;
-    const user = await User.findByIdAndUpdate(id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+    const user = await User.findByIdAndUpdate(
+      req.tokenPayload.userId,
+      req.body,
+      
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
     if (user) {
       res.json(user);
     } else {
       res.status(404).json({ message: "user not found" });
     }
   } catch (error) {
+    console.log(error);
     res.status(400).json({ message: error.message });
   }
 });
